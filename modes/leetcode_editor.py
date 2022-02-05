d1b0c26cdc1a9143021d3932eccc9bc51a0877e0
Reuben John <reubenvjohn@gmail.com>
2022-02-04 23:39:19 -0500

    Working implementation of leetcode-editor.

--- modes/leetcode_editor.py
+++ modes/leetcode_editor.py
@@ -4,15 +4,20 @@ import typing
 
 MODE_IDENTIFIER = 'leetcode-editor'
 
+mean, std = 311, 152
+mean //= 16
+std //= 16
 
 def process(input_file: typing.IO, output: typing.IO, shortcut: str):
     lines = input_file.readlines()
 
     output.write(f"""#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
 ; #Warn  ; Enable warnings to assist with detecting common errors.
+#SingleInstance Force
 SendMode Event  ; Recommended for new scripts due to its superior speed and reliability.
 SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
 
+Escape::Reload
 {shortcut}::GoTo, CMD
 
 CMD:
@@ -24,7 +29,10 @@ CMD:
         for c in stripped:
             if c == ' ':
                 c = 'Space'
-            output.write(f'Sleep, {int(random.normalvariate(311, 152))}{os.linesep}')
+            elif c == '}':
+                c = 'Down'
+
+            output.write(f'Sleep, {int(random.normalvariate(mean, std))}{os.linesep}')
             output.write(f'Send, {{{c}}}{os.linesep}')
         output.write(f'Send, {{Enter}}{os.linesep}')
 
