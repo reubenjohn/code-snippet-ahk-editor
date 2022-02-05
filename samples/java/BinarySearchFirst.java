ae602e8ef5fd3f086d6589317e163d0db112a9c5
Reuben John <reubenvjohn@gmail.com>
2022-02-05 02:51:37 -0500

    Typo in binary search sample java code snippet.

--- samples/java/BinarySearchFirst.java
+++ samples/java/BinarySearchFirst.java
@@ -1,15 +1,15 @@
-public int binarySearchLast(int[] values, int target) {
+    public int binarySearchFirst(int[] values, int target) {
         int start = 0, end = values.length - 1;
-        
+
         int matchIndex = -1;
         while(start <= end) {
             int mid = start + (end - start) / 2;
             if(values[mid] == target) {
                 matchIndex = mid;
-                start = mid + 1;
-            } else if(values[mid] > target) {
                 end = mid - 1;
-            } else if(values[mid] < target) {
+            } else if(target < values[mid]) {
+                end = mid - 1;
+            } else {
                 start = mid + 1;
             }
         }
--- samples/java/BinarySearchLast.java
+++ samples/java/BinarySearchLast.java
@@ -1,14 +1,15 @@
-    public int binarySearchFirst(int[] values, int target) {
+public int binarySearchLast(int[] values, int target) {
         int start = 0, end = values.length - 1;
+
         int matchIndex = -1;
         while(start <= end) {
             int mid = start + (end - start) / 2;
             if(values[mid] == target) {
                 matchIndex = mid;
+                start = mid + 1;
+            } else if(values[mid] > target) {
                 end = mid - 1;
-            } else if(target < values[mid]) {
-                end = mid - 1;
-            } else {
+            } else if(values[mid] < target) {
                 start = mid + 1;
             }
         }
